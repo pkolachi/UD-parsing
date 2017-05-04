28d1cb1881705adb909489774e565213f915a145
Prasanth Kolachina <prasanthk.ms09@gmail.com>
2017-05-04 16:57:11 +0200

    cleaned master branch

--- scripts/PIPE_VARS.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-#export PIPELINE_DIR="${HOME}/Documents/chalmers-work/UD-tasks/UD-parsing";
-export JAVA="/usr/bin/java";
-export CORES="4";
-export MEM="10G";
-#export MODELS_DIR="${PIPELINE_DIR}/models/v1.4"
-
