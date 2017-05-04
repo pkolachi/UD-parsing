5b6b2d1f5ddebf38baa9301c2f7b2570b57e7bb5
Prasanth Kolachina <prasanthk.ms09@gmail.com>
2017-05-04 16:48:41 +0200

    updated with first working version of udjpipe

---
+++ scripts/pipeline.sh
@@ -0,0 +1,158 @@
+#!/bin/bash
+
+OS=`uname`
+# Some machines (older OS X, BSD, Windows environments) don't support readlink -e
+if hash readlink 2>/dev/null; then
+  scriptdir=`dirname $0`
+else
+  scriptpath=$(readlink -e "$0") || scriptpath=$0
+  scriptdir=$(dirname "$scriptpath")
+fi
+WORK_DIR="$(cd "$scriptdir" && pwd -P)"
+source "${WORK_DIR}/PIPE_VARS.sh"
+
+PIPELINE_DIR="${WORK_DIR}/.."
+# mandatory variable defs PIPELINE_DIR, JAVA, MODELS_DIR
+# options variables defs  CORES, MEM
+LIB_DIR="${PIPELINE_DIR}/lib"
+SCRIPT_DIR="${PIPELINE_DIR}/scripts"
+MODELS_DIR="${PIPELINE_DIR}/models/v1.4"
+
+if (( $# < 1)); then
+  echo "Error: $0 lang-code [<tmp-dir>]"
+  exit 1;
+else
+  LANG=$1;
+fi
+if (( $# >= 2)); then
+  TMP_DIR=$2;
+  if [[ ! -d "${TMP_DIR}" ]]; then
+      mkdir -p "${TMP_DIR}";
+  fi
+else
+  TMP_DIR="/tmp";
+fi
+
+function tokenize {
+  TOKENIZER="${LIB_DIR}/tokenizer.perl"
+  tokfile="$1";
+
+  cmd="${TOKENIZER} -l ${LANG} > ${tokfile}";
+  echo $cmd >&2;
+  eval $cmd >&2;
+}
+
+function prepareconll {
+  tokenizedfile="$1";
+  conllfile="$2";
+
+  cmd="${SCRIPT_DIR}/prepare_data.py tokenize ${tokenizedfile} ${conllfile}";
+  echo $cmd >&2;
+  eval $cmd >&2;
+}
+
+function convert2conll09 {
+  inputconllfile="$1";
+  sentsfile="$2";
+  taggerin="$3";
+  lexicon="$4";
+
+  cmd="${SCRIPT_DIR}/prepare_data.py totsv2 ${inputconllfile} ${sentsfile} ${taggerin} ${lexicon}";
+  echo $cmd >&2; 
+  eval $cmd >&2;
+}
+
+function joint_tagger {
+  m_mdlfile=$1;
+  l_mdlfile=$2;
+  infile=$3;
+  outfile=$4;
+
+  TAGGER_JAR="${LIB_DIR}/marmot-2017-04-18.jar";
+  DEPS="${LIB_DIR}/trove-3.1a1.jar:${LIB_DIR}/mallet.jar";
+
+  cmd="java -Xmx${MEM} -classpath ${TAGGER_JAR}:${DEPS} marmot.morph.cmd.Annotator \
+         -model-file $m_mdlfile \
+	 -lemmatizer-file $l_mdlfile \
+	 -test-file form-index=1,token-feature-index=2,$infile \
+	 -pred-file $outfile";
+  echo $cmd >&2; 
+  eval $cmd >&2;
+
+}
+
+function graph_parser {
+  mdlfile=$1;
+  infile=$2;
+  outfile=$3;
+
+  PARSER_JAR="${LIB_DIR}/anna-3.3.jar";
+  DEPS="";
+
+  cmd="${JAVA} -Xmx${MEM} -classpath $PARSER_JAR is2.parser.Parser \
+          -model $mdlfile \
+	  -test  $infile  \
+	  -out   $outfile \
+	  -cores $CORES" ;  
+  echo "$cmd" >&2;
+  eval $cmd   >&2;
+}
+
+function convert2conllu {
+  sentsfile=$1;
+  parserout=$2;
+  
+  cmd="${SCRIPT_DIR}/prepare_data.py toconllu ${sentsfile} ${parserout}";
+  echo "$cmd" >&2; 
+  eval $cmd   >&2;
+}
+
+function add2conll09 {
+  taggerout=$1;
+  parserin=$2; 
+
+  cmd="${SCRIPT_DIR}/prepare_data.py prepmate ${taggerout} ${parserin}";
+  echo "$cmd" >&2; 
+  eval $cmd   >&2;
+}
+
+function remove_tmp {
+  for scratchfile in "$@"
+  do
+      if [[ -f "${scratchfile}" ]]; then
+	  cmd="rm -v ${scratchfile}";
+	  echo "$cmd" >&2;
+	  eval $cmd   >&2;
+      fi
+  done
+}
+
+# -- set variables based on variables defined in PIPE_VARS.sh
+MODEL_DIR="${MODELS_DIR}/${LANG}"
+source ${MODEL_DIR}/lang.sh  # this defines all necessary variables for language. 
+# mandatory variable defs MDL_MTAGGER, MDL_LTAGGER, MDL_GPARSER, MDL_ABBREVS
+# options variables defs  MDL_LEXICON
+MDL_MTAGGER="${MODEL_DIR}/${MDL_MTAGGER}";
+MDL_LTAGGER="${MODEL_DIR}/${MDL_LTAGGER}";
+MDL_GPARSER="${MODEL_DIR}/${MDL_GPARSER}";
+MDL_CLASSES="${MODEL_DIR}/${MDL_CLASSES}";
+MDL_ABBREVS="${MODEL_DIR}/${MDL_ABBREVS}";
+
+# -- temporary storage for stages in the pipeline ;
+TMP_PREFIX="${TMP_DIR}/$$"
+TMP_TOKFILE="${TMP_PREFIX}.tok.txt";
+TMP_INFILE="${TMP_PREFIX}.tok.conllu";
+TMP_SENTSFILE="${TMP_PREFIX}.tok.sents";
+TMP_TAGGERINFILE="${TMP_PREFIX}.taginp.tsv" ;
+TMP_TAGGEDFILE="${TMP_PREFIX}.tagged.conll09";
+TMP_PARSERINFILE="${TMP_PREFIX}.parsinp.conll09";
+TMP_PARSEDFILE="${TMP_PREFIX}.parsed.conll09";
+
+tokenize        $TMP_TOKFILE;
+prepareconll    $TMP_TOKFILE $TMP_INFILE;
+convert2conll09 $TMP_INFILE $TMP_SENTSFILE $TMP_TAGGERINFILE $MDL_CLASSES;
+joint_tagger    $MDL_MTAGGER $MDL_LTAGGER $TMP_TAGGERINFILE $TMP_TAGGEDFILE ;
+add2conll09     $TMP_TAGGEDFILE $TMP_PARSERINFILE ;
+graph_parser    $MDL_GPARSER $TMP_PARSERINFILE $TMP_PARSEDFILE ;
+convert2conllu  $TMP_SENTSFILE $TMP_PARSEDFILE ;
+remove_tmp      $TMP_TOKFILE $TMP_INFILE $TMP_SENTSFILE $TMP_TAGGERINFILE $TMP_TAGGEDFILE $TMP_PARSERINFILE $TMP_PARSEDFILE ;
---
+++ scripts/prepare_data.py
@@ -0,0 +1,317 @@
+#!/usr/bin/env python3
+
+from collections import defaultdict;
+from operator import itemgetter;
+from sys import argv as sysargv, stdin, stdout, stderr, exit as sysexit;
+import itertools as it;
+import io;
+import os.path as pathutils;
+import re;
+
+WSPAT  = re.compile('\s+', flags=re.U);
+TABPAT = re.compile('\t',  flags=re.U);
+BUF_SIZE = 10000;
+
+def smart_open(filename='', mode='r', large=True):
+  from bz2 import BZ2File;
+  from gzip import GzipFile;
+  
+  bufferSize = ((2<<16)+8) if large == True else io.DEFAULT_BUFFER_SIZE;
+  filename = filename.strip();
+  if filename:
+    _, ext = pathutils.splitext(filename);
+    if ext == '.bz2':
+      mode = 'rb' if mode in ['r', 'rb'] else 'wb';
+      iostream = BZ2File(filename,  mode=mode, buffering=bufferSize);
+    elif ext == '.gz':
+      mode = 'rb' if mode in ['r', 'rb'] else 'wb';
+      iostream = GzipFile(filename, mode=mode);
+    else:
+      mode = 'rb' if mode in ['r', 'rb'] else 'wb';
+      iostream = io.open(filename,  mode=mode, buffering=bufferSize);
+  else:
+    iostream = stdin.buffer if mode in ['r', 'rb'] else stdout.buffer;
+
+  return io.TextIOWrapper(iostream, encoding='utf-8');
+
+def lines_from_file(filename, large=False, batchsize=0):
+  with smart_open(filename, large=large) as infile:
+    line_count = 0;
+    for line_count, line in enumerate(infile, start=1):
+      yield line.strip();
+  return;
+
+def lines_to_file(filename, lines, batchsize=0):
+  with smart_open(filename, mode='w') as outfile:
+    line_count = 0;
+    for line_count, sent in enumerate(lines, start=1):
+      print(sent.strip(), file=outfile);
+  return True;
+
+# These are the labels on the columns in the CoNLL 2007 dataset.
+CONLL07_COLUMNS = (
+    'id', 'form', 'lemma',
+    'cpostag', 'postag', 'feats',
+    'head', 'deprel',
+    'phead', 'pdeprel',
+    );
+# These are the labels on the columns in the CoNLL 2009 dataset.
+CONLL09_COLUMNS = (
+    'id', 'form', 'lemma', 'plemma',
+    'postag', 'ppostag', 'feats', 'pfeats',
+    'head', 'phead', 'deprel', 'pdeprel', 'fillpred', 'sense', 
+    );
+# These are the labels on the columns in the ConllU format (UD treebanks).
+CONLLU_COLUMNS = (
+    'id', 'form', 'lemma',
+    'postag', 'xpostag', 'feats',
+    'head', 'deprel',
+    'deps', 'misc', 
+    );
+
+# These are the labels on the columns when Berkeley parser 
+# is given pre-tagged input
+BERKELEY_COLUMNS = ('form', 'postag', );
+# These are the labels on the output of morfette tagger
+MORFETTE_COLUMNS = ('form', 'lemma', 'postag', );
+TSV_INPUT = ('id', 'form', );
+
+FIELDS = CONLLU_COLUMNS;
+
+def words_from_conll(lines, fields=CONLLU_COLUMNS):
+  '''Read words for a single sentence from a CoNLL text file.'''
+  # using this with filter doubles parsing time 
+  def isMultiWord(x): return re.match('^[0-9]+?-[0-9]+?$', x);
+  def parseFeats(fstruc): 
+    return tuple(
+      tuple(x.split('=', 1)) for x in fstruc.split('|')
+    );
+
+  for line in lines:
+    entries = re.split(TABPAT, line);
+    if fields == CONLLU_COLUMNS and isMultiWord(entries[0]):
+      continue;
+    entries = zip(fields, entries);
+    entry = defaultdict(lambda: u'_', entries);
+
+    if 'feats' in fields and entry['feats'] != '_':
+      entry['feats'] = parseFeats(entry['feats']);
+    if 'pfeats' in fields and entry['pfeats'] != '_':
+      entry['pfeats'] = parseFeats(entry['pfeats']);
+
+    yield entry;
+
+def words_to_conll(sent, fields=CONLLU_COLUMNS):
+  str_repr = [];
+  if type(sent) == type(()) and len(sent) == 2:
+    str_repr.append(str(sent[0]));
+    sent = sent[1];
+  for token in sent:
+    feat_repr = \
+        '|'.join('{0}={1}'.format(feat, value) for feat, value in token['feats']) \
+        if 'feats' in token and type(token['feats']) == type(()) \
+        else token['feats'];
+    token['feats'] = feat_repr if feat_repr.strip() else '_';
+    if 'pfeats' in token:
+      feat_repr = \
+        '|'.join('{0}={1}'.format(feat, value) for feat, value in token['pfeats']) \
+        if 'pfeats' in token and type(token['pfeats']) == type(()) \
+        else token['pfeats'];
+      token['pfeats'] = feat_repr if feat_repr.strip() else '_';
+    str_repr.append('\t'.join(token[feat] for feat in fields));
+  return '\n'.join(str_repr);
+
+def lines_from_conll(lines):
+  '''Read lines for a single sentence from a CoNLL text file.'''
+  for line in lines:
+    if not line.strip():
+      return;
+    yield line.strip();
+
+def sentences_from_conll(stream, fields=None):
+  '''Read sentences from lines in an open CoNLL file handle.'''
+  global FIELDS;
+  if not fields: 
+    fields = FIELDS;
+  sent_count = 0;
+  while True:
+    lines = tuple(lines_from_conll(stream));
+    if not len(lines):
+      break;
+    sent_count += 1;
+    comm_lines  = it.takewhile(lambda X: X.startswith('#'), lines);
+    comm_lines  = '\n'.join(comm_lines); 
+    conll_lines = it.dropwhile(lambda X: X.startswith('#'), lines);
+    tree = list(words_from_conll(conll_lines, fields=fields));
+    yield (comm_lines, tree);
+  return;
+
+def sentences_to_conll(sentences, fields=None):
+  global FIELDS;
+  if not fields:
+    fields = FIELDS;
+  for sent_count, sent in enumerate(sentences, start=1):
+    yield words_to_conll(sent, fields=fields);
+    yield "";
+
+def swapConllFields(oldColumns, newColumns, remove=False):
+  def worker(conll_sent):
+    if type(conll_sent) == type(()) and len(conll_sent) == 2:
+      meta_info  = conll_sent[0]
+      conll_sent = conll_sent[1];
+    else:
+      meta_info  = '';
+    for edge in conll_sent:
+      for old, new in zip(oldColumns, newColumns):
+        edge[new] = edge[old];
+      if remove:
+        for old in oldColumns:
+          if old in edge:
+            del edge[old];
+    return (meta_info, conll_sent); 
+  return worker ;
+
+def toconll09(inputfile='', outputfile='', source='gold'):
+  systemConv = swapConllFields(['postag', 'lemma', 'feats', 'head', 'deprel'], ['ppostag', 'plemma', 'pfeats', 'phead', 'pdeprel'], remove=True);
+  goldConv   = swapConllFields(['postag', 'lemma', 'feats', 'head', 'deprel'], ['postag', 'lemma', 'feats', 'head', 'deprel'], remove=False);
+
+  transformer  = goldConv if source == 'gold' else systemConv ;
+  conll_sents  = sentences_from_conllfile(inputfile);
+  nconll_sents = map(transformer, conll_sents);
+  sentences_to_conllfile(outputfile, nconll_sents, fields=CONLL09_COLUMNS);
+  return;
+
+def toconll07(inputfile='', outputfile='', source=''):
+  global FIELDS;
+  if True or FIELDS == CONLL09_COLUMNS:
+    systemConv = swapConllFields(['plemma', 'ppostag', 'ppostag', 'pfeats', 'phead', 'pdeprel'], ['lemma', 'postag', 'cpostag', 'feats', 'head', 'deprel'], remove=True);
+    goldConv   = swapConllFields(['lemma', 'postag', 'postag', 'feats', 'head', 'deprel'], ['lemma', 'postag', 'cpostag', 'feats', 'head', 'deprel'], remove=False);
+    blindConv  = swapConllFields(['plemma', 'ppostag', 'ppostag', 'pfeats'], ['lemma', 'postag', 'cpostag', 'feats'], remove=True);
+  
+  transformer  = goldConv if source == 'gold' else blindCov if source == 'blind' else systemConv ;
+  conll_sents  = sentences_from_conllfile(inputfile);
+  nconll_sents = map(transformer, conll_sents);
+  sentences_to_conllfile(outputfile, nconll_sents, fields=CONLL07_COLUMNS);
+  return;
+
+def toconllu(inputfile='', outputfile='', source=''):
+  global FIELDS;
+  if True or FIELDS == CONLL09_COLUMNS:
+    systemConv = swapConllFields(['plemma', 'ppostag', 'ppostag', 'pfeats', 'phead', 'pdeprel'], ['lemma', 'postag', 'xpostag', 'feats', 'head', 'deprel'], remove=True);
+    goldConv   = swapConllFields(['lemma', 'postag', 'postag', 'feats', 'head', 'deprel'], ['lemma', 'postag', 'xpostag', 'feats', 'head', 'deprel'], remove=False);
+    blindConv  = swapConllFields(['plemma', 'ppostag', 'ppostag', 'pfeats'], ['lemma', 'postag', 'xpostag', 'feats'], remove=True);
+  
+  transformer  = goldConv if source == 'gold' else blindCov if source == 'blind' else systemConv ;
+  conll_sents  = sentences_from_conllfile(inputfile);
+  nconll_sents = map(transformer, conll_sents);
+  sentences_to_conllfile(outputfile, nconll_sents, fields=CONLLU_COLUMNS);
+  return;
+
+
+def tokenized_to_sentences(sentences):
+  global FIELDS;
+  for sent_idx, sent in enumerate(sentences, start=1):
+    if not sent.strip():
+      continue;
+    tokens = re.split('\s+', sent.strip());
+    if not len(tokens):
+      print('warning: empty sentence at %d' %(sent_idx), file=stderr);
+      continue;
+    conll_sent = [defaultdict(lambda: '_', {'form': tok, 'id': str(tok_idx)})
+        for tok_idx, tok in enumerate(tokens, start=1)];
+    yield ("#metainfo: {0}".format(sent.strip()), conll_sent);
+
+
+# -- Higher-level functions to be accessed from other functions
+
+def sentences_from_conllfile(filename, fields=None):
+  if not fields:
+    global FIELDS;
+    fields = FIELDS;
+  lines = lines_from_file(filename);
+  return sentences_from_conll(lines, fields=fields);
+
+def sentences_to_conllfile(filename, sentences, fields=None):
+  if not fields:
+    global FIELDS;
+    fields = FIELDS;
+  return lines_to_file(filename, sentences_to_conll(sentences, fields=fields));
+
+def readLexicon(wordclasses):
+  lines = lines_from_file(wordclasses);
+  lexicon = map(lambda X: tuple(re.split(WSPAT, X, maxsplit=1)), lines);
+  lexicon = map(lambda pair: (pair[0], re.sub('\s+', '#', pair[1]).replace(':', '_')), lexicon);
+  lexicon = dict(lexicon);
+  lexicon = defaultdict(lambda: lexicon['<RARE>'], lexicon);
+  return lexicon;
+
+def addClasses(lexicon):
+  def worker(conll_sent):
+    if type(conll_sent) == type(()) and len(conll_sent) == 2:
+      meta_info  = conll_sent[0];
+      conll_sent = conll_sent[1];
+    else:
+      meta_info  = '';
+    
+    for edge in conll_sent:
+      edge['CLASS'] = lexicon[edge['form'].lower()];
+    return conll_sent;
+  return worker;
+
+def prepare_lemming_data(inputfile='', sentsfile='', outputfile='', lexicon=''):
+  conll_sents  = sentences_from_conllfile(inputfile);
+  if lexicon.strip():
+    classes = readLexicon(lexicon);
+    transformer = addClasses(classes);
+    nconll_sents = map(transformer, conll_sents);
+  else:
+    nconll_sents = conll_sents;
+
+  sentences_to_conllfile(outputfile, nconll_sents, fields=('id', 'form', 'CLASS'));
+  return;
+
+def prepare_mate_data(inputfile='', outputfile=''):
+  global CONLL09_COLUMNS;
+  conll_sents = sentences_from_conllfile(inputfile, fields=CONLL09_COLUMNS);
+  sentences_to_conllfile(outputfile, conll_sents, fields=CONLL09_COLUMNS);
+
+def reorganize_data(inputfile='', sentsfile='', outputfile=''):
+  global FIELDS;
+  FIELDS = CONLL09_COLUMNS;
+  toconllu(inputfile, outputfile, source='system');
+  return;
+
+def prepare_lemming(inputfile='', outputfile='', lexicon=''):
+  conll_sents  = sentences_from_conllfile(inputfile);
+  if lexicon.strip():
+    classes = readLexicon(lexicon);
+    transformer = addClasses(classes);
+    nconll_sents = map(transformer, conll_sents);
+  else:
+    nconll_sents = conll_sents;
+
+  sentences_to_conllfile(outputfile, nconll_sents, fields=('id', 'form', 'lemma', 'postag', 'feats', 'CLASS'));
+  return;
+
+def prepare_conllinput(inputfile='', outputfile=''):
+  sents = list(lines_from_file(inputfile));
+  conll_sents = tokenized_to_sentences(sents);
+  sentences_to_conllfile(outputfile, conll_sents, fields=CONLLU_COLUMNS);
+  return;
+
+if __name__ == '__main__':
+  if sysargv[1] == 'totsv':
+    classesfile = '' if len(sysargv) < 5 else sysargv[4];
+    prepare_lemming_data(sentsfile=sysargv[2], outputfile=sysargv[3], lexicon=classesfile);
+  elif sysargv[1] == 'totsv2':
+    classesfile = '' if len(sysargv) < 6 else sysargv[5];
+    prepare_lemming_data(inputfile=sysargv[2], sentsfile=sysargv[3], outputfile=sysargv[4], lexicon=classesfile);
+  elif sysargv[1] == 'toconllu':
+    reorganize_data(sentsfile=sysargv[2], inputfile=sysargv[3]);
+  elif sysargv[1] == 'traintagger':
+    classesfile = '' if len(sysargv) < 5 else sysargv[4];
+    prepare_lemming(inputfile=sysargv[2], outputfile=sysargv[3], lexicon=sysargv[4]);
+  elif sysargv[1] == 'prepmate':
+    prepare_mate_data(inputfile=sysargv[2], outputfile=sysargv[3]);
+  elif sysargv[1] == 'tokenize':
+    prepare_conllinput(inputfile=sysargv[2], outputfile=sysargv[3]);
