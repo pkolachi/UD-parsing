b44852247a10d6ed618797ffe3f88ed638fc5e60
Prasanth Kolachina <prasanthk.ms09@gmail.com>
2017-05-04 21:57:20 +0200

    no changes

--- scripts/PIPE_VARS.sh
+++ scripts/PIPE_VARS.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 #export PIPELINE_DIR="${HOME}/Documents/chalmers-work/UD-tasks/UD-parsing";
+#export MODELS_DIR="${PIPELINE_DIR}/models/v1.4"
 export JAVA="/usr/bin/java";
 export CORES="4";
 export MEM="10G";
-#export MODELS_DIR="${PIPELINE_DIR}/models/v1.4"
 
